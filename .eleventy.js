7ce1d1e4f1b1f86c162b7e3a5d96feef72a37d17
deannabosschert <deanna.bosschert@gmail.com>
2020-06-23 18:21:27 +0200

    feat: add eleventy config file

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filesize = require("file-size");
+
+module.exports = (function(eleventyConfig) {
+ eleventyConfig.addFilter("filesize", function(path) {
+  let stat = fs.statSync(path);
+  if( stat ) {
+   return filesize(stat.size).human();
+  }
+  return "";
+ });
+});
